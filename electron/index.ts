aa111221f315796f27d6edbccefe7cdcb439c459
ManoelJunim <manoelfpjunior@hotmail.com>
2022-05-27 21:57:33 -0300

    design chenged

--- electron/index.ts
+++ electron/index.ts
@@ -17,7 +17,7 @@ const createWindow = (): void => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     height: 600,
-    width: 800,
+    width: 900,
     webPreferences: {
       preload: MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY,
     },
